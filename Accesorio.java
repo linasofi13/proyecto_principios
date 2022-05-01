fbc349e664faedebddbd81b0fc0dcc3f38f69b6c
Lina <103126242+linasofi13@users.noreply.github.com>
2022-05-01 16:50:12 -0500

    Update Accesorio.java

--- Accesorio.java
+++ Accesorio.java
@@ -1,165 +1,278 @@
-import java.util.Scanner;
 import java.util.ArrayList;
-
-public class Principal{		
+import java.util.Scanner;
+public class Accesorio {
 	
-	public static ArrayList<Cuenta> cuentas = new ArrayList<Cuenta>();
-	public static ArrayList<Accesorio> accesoriosComprados = new ArrayList<Accesorio>();
-	public static Accesorio[] prendas = new Accesorio[10];
-	public static Accesorio[] macetas = new Accesorio[5];
-	public static Accesorio[] fondos = new Accesorio[5];	
+	private String nombre;
+	private int precio;
 	
-	public static void main(String[] args) {
-		
-		Planta planta1 = new Planta("nombre", 0, 0, 0);
-		
-		prendas[0] = new Accesorio("Sombrero Verde", 75);
-		prendas[1] = new Accesorio("Sombrero Blanco", 100);
-		prendas[2] = new Accesorio("Collar Brillante", 200);
-		prendas[3] = new Accesorio("Collar de Esmeraldas", 175);
-		prendas[4] = new Accesorio("Gafas de Corazón", 215);
-		prendas[5] = new Accesorio("Gafas de Sol Negras", 130);
-		prendas[6] = new Accesorio("Gafas de Sol Moradas", 150);
-		prendas[7] = new Accesorio("Bufanda Rayada", 190);
-		prendas[8] = new Accesorio("Bufanda Rosada", 230);
-		prendas[9] = new Accesorio("Gorra Anaranjada", 110);
-		
-		macetas[0] = new Accesorio("Maceta Verde", 50);
-		macetas[1] = new Accesorio("Maceta Azúl", 175);	
-		macetas[2] = new Accesorio("Maceta Rosada", 100);	
-		macetas[3] = new Accesorio("Maceta Amarilla", 120);	
-		macetas[4] = new Accesorio("Maceta Roja", 130);	
-		
-		fondos[0] = new Accesorio("Fondo Blanco", 80);	
-		fondos[1] = new Accesorio("Fondo Verde", 50);	
-		fondos[2] = new Accesorio("Fondo Azúl", 120);	
-		fondos[3] = new Accesorio("Fondo Amarillo", 100);	
-		fondos[4] = new Accesorio("Fondo Café", 100);	
-		
-		
-		Scanner scan = new Scanner(System.in);
-			
-		System.out.println("------------------------------------ECOPLAY---------------------------------------");
-		System.out.println("Bienvenido a EcoPlay,"
-				+ " para ingresar al juego, debe registrarse creando una cuenta.");
-		System.out.println("----------------------------------------------------------------------------------");
-		System.out.println("Seleccione una opción:");
-		System.out.println("0 | CREAR NUEVA CUENTA");
-		System.out.println("1 | SALIR");
-		Cuenta.crearNuevaCuenta();
-		System.out.println("¡Bienvenidx, "+ cuentas.get(0).getNombreCuenta() + "!");
-		System.out.println("                    _\r\n"
-				+ "                  _(_)_                          wWWWw   _\r\n"
-				+ "      @@@@       (_)@(_)   vVVVv     _     @@@@  (___) _(_)_\r\n"
-				+ "     @@()@@ wWWWw  (_)\\    (___)   _(_)_  @@()@@   Y  (_)@(_)\r\n"
-				+ "      @@@@  (___)     `|/    Y    (_)@(_)  @@@@   \\|/   (_)\\\r\n"
-				+ "       /      Y       \\|    \\|/    /(_)    \\|      |/      |\r\n"
-				+ "    \\ |     \\ |/       | / \\ | /  \\|/       |/    \\|      \\|/\r\n"
-				+ "   \\\\|//   \\\\|///  \\\\\\|//\\\\\\|/// \\|///  \\\\\\|//  \\\\|//  \\\\\\|// \r\n"
-				+ "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
-		System.out.println("Ahora, vamos a adoptar una planta :)");
-		Planta.adoptarPlanta(planta1);
-		System.out.println("¡Perfecto! Ahora podrás empezar a jugar y cuidar de tu planta");
-		System.out.println(" _,-._\r\n"
-				+ "/ \\_/ \\\r\n"
-				+ ">-(_)-<    \r\n"
-				+ "\\_/ \\_/\r\n"
-				+ "  `-'");
-		System.out.println("°---Ahora vas a encontrar el menú principal, te recomendamos que primero sigas el comando que especifica las instrucciones del juego.---°");
-		System.out.println("Presiona cualquier número del teclado para continuar. ");
-		int teclado = scan.nextInt();
-		Principal.mostrarMenu(planta1, prendas, macetas, fondos, cuentas, accesoriosComprados);
-		
-		
+	public Accesorio(String nombre, int precio){
+		this.nombre = nombre;
+		this.precio = precio;
 	}
-	public static void mostrarMenu(Planta planta1, Accesorio [] prendas, Accesorio [] macetas, Accesorio [] fondos, ArrayList<Cuenta> cuentas, ArrayList<Accesorio> accesoriosComprados) {
+	public void setNombreAccesorio(String nombre) { //no estoy segura si los métodos set son necesarios en esta clase, ya que los valores ya vienen predefinidos.
+		this.nombre = nombre;
+	}
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+	public String getNombreAccesorio() {
+		return this.nombre;
+	}
+	public int getPrecio() {
+		return this.precio;
+	}
+	public String toString(){
+        String infoAccesorio = "Accesorio: "+this.getNombreAccesorio() +" | Precio: $"+this.getPrecio() + "\n";
+        return infoAccesorio;
+    }
+	public static void personalizarPlanta(Accesorio[] prendas, Accesorio[] macetas, Accesorio[] fondos, ArrayList<Cuenta> cuentas, ArrayList<Accesorio> accesoriosComprados) {
 		
 		Scanner scan = new Scanner(System.in);
 		
-		while(true) {
-		 System.out.println("\n|------------------------------------------------------------------------------|");
-         System.out.println("|------------------------------MENÚ PRINCIPAL-----------------------------------|");
-         System.out.println("|" + planta1.toString() +"|");
-         System.out.println("|-------------------------------------------------------------------------------|");
-         System.out.println("|                        ~~Selecciona lo que quieres hacer~~                    |");
-         System.out.println("| 0. Cerrar el Programa.                                                        |");
-         System.out.println("| 1. Instrucciones de Juego.                                                    |");
-         System.out.println("| 2. Visualizar información de la cuenta.                                       |");
-         System.out.println("| 3. Visualizar Nivel de Jugador.                                               |");
-         System.out.println("| 4. Visualizar Cantidad de EXP.                                                |");
-         System.out.println("| 5. Visualizar Cantidad de Soles $$$.                                          |");
-         System.out.println("| 6. Regar Planta.                                                              |");
-         System.out.println("| 7. Exponer al Sol a la Planta.                                                |");
-         System.out.println("| 8. Desparasitar a la Planta.                                                  |");
-         System.out.println("| 9. Personalizar la Planta.                                                    |");
-         System.out.println("| 10. Visualizar Accesorios Comprados.                                       |");
-         int opcion = scan.nextInt(); 
-         System.out.println("|-------------------------------------------------------------------------------|\n\n\n");
-         
-         if(opcion == 0){
-             System.out.println("El programa se ha cerrado correctamente.");
-             System.out.println("¡Vuelve Pronto!");
-             break;
+		System.out.println("-----Bienvenidx a la tienda de accesorios de EcoPlay, con tus Soles puedes comprar accesorios.-----");
+		System.out.println("Los accesorios se clasifican en prendas para que vistas a tu planta, macetas para la tierra de tu planta y fondos de la pared en la que está tu planta.");
+		System.out.println("---Pulsa 0 para continuar a la tienda---");
+		System.out.println("---Pulsa 1 para volver al menú principal---");
+		int opcion = scan.nextInt();
+		if(opcion == 0){ //continuar a la tienda
+			Accesorio.visualizarTienda();
+			int opcion2 = scan.nextInt();
+			if(opcion2 == 1){ //escoge VER PRENDAS 
+			for(int i = 0; i < prendas.length ; i++) {
+				System.out.println(prendas[i].toString());
+				
+			}
+			System.out.println("Pulsa: \n");
+			System.out.println("1 | COMPRAR UN ACCESORIO");
+			System.out.println("2 | REGRESAR");
+			int opcion3 = scan.nextInt();
+			if(opcion3 == 1) { //comprar un accesorio prendas
+				Accesorio.comprarAccesorioPrendas(prendas, cuentas, accesoriosComprados); //check
+			}
+			else if(opcion3 == 2) { //regresa
+				Accesorio.visualizarTienda();
+				int opcion4 = scan.nextInt();
+				
+			}
+			}else if(opcion2 == 2) { //escoger VER MACETAS
+				for(int i = 0; i < macetas.length ; i++) { 
+					System.out.println(macetas[i].toString());
+			}
+				System.out.println("Pulsa: \n");
+				System.out.println("1 | COMPRAR UN ACCESORIO");
+				System.out.println("2 | REGRESAR");
+				int opcion3 = scan.nextInt();
+				if(opcion3 == 1) { //compra un accesorio maceta
+					Accesorio.comprarAccesorioMacetas(macetas, cuentas, accesoriosComprados); //check
+				}
+				else if(opcion3 == 2) { //regresa
+					Accesorio.visualizarTienda();
+					int opcion4 = scan.nextInt();
+					
+				}
+			}else if(opcion2 == 3) { //escoge VER FONDOS 
+				for(int i = 0; i < fondos.length ; i++) { // TO DO
+					System.out.println(fondos[i].toString());								
+			}System.out.println("Pulsa: \n");
+			System.out.println("1 | COMPRAR UN ACCESORIO");
+			System.out.println("2 | REGRESAR");
+			int opcion3 = scan.nextInt();
+			if(opcion3 == 1) { //compra un accesorio maceta
+				Accesorio.comprarAccesorioFondos(fondos, cuentas, accesoriosComprados); //check
+			}
+			else if(opcion3 == 2) { //regresa
+				Accesorio.visualizarTienda();
+				int opcion4 = scan.nextInt();
+				
+			}
+		}else if(opcion2 == 4) {
+			Accesorio.personalizarPlanta(prendas, macetas, fondos, cuentas, accesoriosComprados);
+		}
+		
+	}	
+}
+	public static void visualizarTienda() {
+		System.out.println("------Bienvenido a la Tienda de Accesorios----\n");
+		System.out.println("Pulsa para ver: \n");
+		System.out.println("1 | PRENDAS");
+		System.out.println("2 | MACETAS");
+		System.out.println("3 | FONDOS");
+		System.out.println("4 | REGRESAR");
+	}
+	public static void comprarAccesorioPrendas(Accesorio[] prendas, ArrayList<Cuenta> cuentas, ArrayList<Accesorio> accesoriosComprados) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("¿Qué Accesorio deseas comprar? \n");
+		for(int j = 0; j < prendas.length; j++) { // preguntar profe
+			for(int i = 0; i < prendas.length ; i++) {
+				System.out.println(j+" | " +prendas[i].toString());
+				j = i;
+			}
+		}
+		int entrada = scan.nextInt();
+		switch(entrada) {
+		case 0:
+			Accesorio.comprobarPagoPrendas(prendas, cuentas, accesoriosComprados, 0);
+		case 1:
+			Accesorio.comprobarPagoPrendas(prendas, cuentas, accesoriosComprados, 1);
+			break;
+		case 2:
+			Accesorio.comprobarPagoPrendas(prendas, cuentas, accesoriosComprados,2);
+			break;
+		case 3:
+			Accesorio.comprobarPagoPrendas(prendas, cuentas, accesoriosComprados,3);
+			break;
+		case 4:
+			Accesorio.comprobarPagoPrendas(prendas, cuentas, accesoriosComprados,4);
+			break;
+		case 5:
+			Accesorio.comprobarPagoPrendas(prendas, cuentas, accesoriosComprados,5);
+			break;
+		case 6:
+			Accesorio.comprobarPagoPrendas(prendas, cuentas, accesoriosComprados,6);
+			break;
+		case 7:
+			Accesorio.comprobarPagoPrendas(prendas, cuentas, accesoriosComprados,7);
+			break;
+		case 8:
+			Accesorio.comprobarPagoPrendas(prendas, cuentas, accesoriosComprados,8);
+			break;
+		case 9:
+			Accesorio.comprobarPagoPrendas(prendas, cuentas, accesoriosComprados,9);
+			break;
+		case 10:
+			Accesorio.comprobarPagoPrendas(prendas, cuentas, accesoriosComprados,10);
+			break;
+		default:
+			System.out.println("Opción no válida");
+			break;
+		}
+		}		
+	public static void comprobarPagoPrendas(Accesorio[] prendas, ArrayList<Cuenta> cuentas, ArrayList<Accesorio> accesoriosComprados, int num) {
+		Scanner scan = new Scanner(System.in);
+		if(cuentas.get(0).getCantidadSoles() >= prendas[num].getPrecio()){
+			System.out.println("¡Has comprado --> " + prendas[num].getNombreAccesorio());
+			accesoriosComprados.add(prendas[num]);
+			System.out.println("1 | COMPRAR OTRO ACCESORIO");
+			System.out.println("2 | VOLVER A LA TIENDA");
+			int entrada2 = scan.nextInt();
+			if(entrada2 == 1){
+				Accesorio.comprarAccesorioPrendas(prendas, cuentas, accesoriosComprados);
+			}
+			else if(entrada2 == 2) {
+				Accesorio.visualizarTienda();
+				int opcion4 = scan.nextInt();
+	}
+		}else {
+			System.out.println("No cuentas con Soles suficientes.");
+		}
 		
 	}
-         switch(opcion){
-         case 1:
-        	 Cuenta.visualizarInstrucciones();
-        	 System.out.println("Pulsa cualquier número del teclado para volver al menú principal.");
-        	 int continuar = scan.nextInt();
-        	 break;
-         case 2:
-        	 Cuenta.visualizarCuenta();
-        	 System.out.println("Pulsa cualquier número del teclado para volver al menú principal.");
-        	 int continuar2 = scan.nextInt();
-        	 break;
-         case 3:
-        	 Cuenta.visualizarNivel();
-        	 System.out.println("Pulsa cualquier número del teclado para volver al menú principal.");
-        	 int continuar3 = scan.nextInt();
-        	 break;
-         case 4:
-        	 Cuenta.visualizarCantidadEXP();
-        	 System.out.println("Pulsa cualquier número del teclado para volver al menú principal.");
-        	 int continuar4 = scan.nextInt();
-        	 break;
-         case 5:
-        	 Cuenta.visualizarCantidadSoles();
-        	 System.out.println("Pulsa cualquier número del teclado para volver al menú principal.");
-        	 int continuar5 = scan.nextInt();
-        	 break;
-         case 6:
-        	 Planta.regarPlanta(planta1);
-        	 System.out.println("Pulsa cualquier número del teclado para volver al menú principal.");
-        	 int continuar6 = scan.nextInt();
-        	 break;
-         case 7:
-        	 Planta.exponerAlSol(planta1);
-        	 System.out.println("Pulsa cualquier número del teclado para volver al menú principal.");
-        	 int continuar8 = scan.nextInt();
-        	 break;
-         case 8:
-        	 Planta.desparasitarPlanta(planta1);
-        	 System.out.println("Pulsa cualquier número del teclado para volver al menú principal.");
-        	 int continuar9 = scan.nextInt();
-        	 break;
-         case 9:
-        	 Accesorio.personalizarPlanta(prendas, macetas, fondos, cuentas, accesoriosComprados);
-        	 System.out.println("Pulsa cualquier número del teclado para volver al menú principal.");
-        	 int continuar10 = scan.nextInt();  
-        	 break;
-         case 10:
-        	 Accesorio.visualizarAccesoriosComprados(accesoriosComprados);
-        	 System.out.println("Pulsa cualquier número del teclado para volver al menú principal.");
-        	 int continuar11 = scan.nextInt();  
-        	 break;
-        default:
-        	System.out.println("Opción no válida.");
-        	System.out.println("Pulsa cualquier número del teclado para volver al menú principal.");
-        	int continuar12 = scan.nextInt();  
-        	
-         }
+	public static void comprarAccesorioMacetas(Accesorio[] macetas, ArrayList<Cuenta> cuentas, ArrayList<Accesorio> accesoriosComprados) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("¿Qué Accesorio deseas comprar? \n");
+		for(int j = 0; j < macetas.length; j++) { // preguntar profe
+			for(int i = 0; i < macetas.length ; i++) {
+				System.out.println(j+" | " + macetas[i].toString());
+				j = i;
+			}
+		}
+		int entrada = scan.nextInt();
+		switch(entrada) {
+		case 0:
+			Accesorio.comprobarPagoMacetas(macetas, cuentas, accesoriosComprados, 0);
+		case 1:
+			Accesorio.comprobarPagoMacetas(macetas, cuentas, accesoriosComprados, 1);
+			break;
+		case 2:
+			Accesorio.comprobarPagoMacetas(macetas, cuentas, accesoriosComprados,2);
+			break;
+		case 3:
+			Accesorio.comprobarPagoMacetas(macetas, cuentas, accesoriosComprados,3);
+			break;
+		case 4:
+			Accesorio.comprobarPagoMacetas(macetas, cuentas, accesoriosComprados,4);
+			break;
+		case 5:
+			Accesorio.comprobarPagoMacetas(macetas, cuentas, accesoriosComprados,5);
+			break;
+		default:
+			System.out.println("Opción no válida");
+			break;
+		}
+		}		
+	public static void comprobarPagoMacetas(Accesorio[] macetas, ArrayList<Cuenta> cuentas, ArrayList<Accesorio> accesoriosComprados, int num) {
+		Scanner scan = new Scanner(System.in);
+		if(cuentas.get(0).getCantidadSoles() >= macetas[num].getPrecio()){
+			System.out.println("¡Has comprado --> " + macetas[num].getNombreAccesorio());
+			accesoriosComprados.add(macetas[num]);
+			System.out.println("1 | COMPRAR OTRO ACCESORIO");
+			System.out.println("2 | VOLVER A LA TIENDA");
+			int entrada2 = scan.nextInt();
+			if(entrada2 == 1){
+				Accesorio.comprarAccesorioMacetas(macetas, cuentas, accesoriosComprados);
+			}
+			else if(entrada2 == 2) {
+				Accesorio.visualizarTienda();
+				int opcion4 = scan.nextInt();
 	}
-
+		}else {
+			System.out.println("No cuentas con Soles suficientes.");
+		}
+}public static void comprarAccesorioFondos(Accesorio[] fondos, ArrayList<Cuenta> cuentas, ArrayList<Accesorio> accesoriosComprados) {
+	Scanner scan = new Scanner(System.in);
+	System.out.println("¿Qué Accesorio deseas comprar? \n");
+	for(int j = 0; j < fondos.length; j++) { // preguntar profe
+		for(int i = 0; i < fondos.length ; i++) {
+			System.out.println(j+" | " + fondos[i].toString());
+			j = i;
+		}
+	}
+	int entrada = scan.nextInt();
+	switch(entrada) {
+	case 0:
+		Accesorio.comprobarPagoFondos(fondos, cuentas, accesoriosComprados, 0);
+	case 1:
+		Accesorio.comprobarPagoFondos(fondos, cuentas, accesoriosComprados, 1);
+		break;
+	case 2:
+		Accesorio.comprobarPagoFondos(fondos, cuentas, accesoriosComprados,2);
+		break;
+	case 3:
+		Accesorio.comprobarPagoFondos(fondos, cuentas, accesoriosComprados,3);
+		break;
+	case 4:
+		Accesorio.comprobarPagoFondos(fondos, cuentas, accesoriosComprados,4);
+		break;
+	case 5:
+		Accesorio.comprobarPagoFondos(fondos, cuentas, accesoriosComprados,5);
+		break;
+	default:
+		System.out.println("Opción no válida");
+		break;
+	}
+	}		
+public static void comprobarPagoFondos(Accesorio[] fondos, ArrayList<Cuenta> cuentas, ArrayList<Accesorio> accesoriosComprados, int num) {
+	Scanner scan = new Scanner(System.in);
+	if(cuentas.get(0).getCantidadSoles() >= fondos[num].getPrecio()){
+		System.out.println("¡Has comprado --> " + fondos[num].getNombreAccesorio());
+		accesoriosComprados.add(fondos[num]);
+		System.out.println("1 | COMPRAR OTRO ACCESORIO");
+		System.out.println("2 | VOLVER A LA TIENDA");
+		int entrada2 = scan.nextInt();
+		if(entrada2 == 1){
+			Accesorio.comprarAccesorioFondos(fondos, cuentas, accesoriosComprados);
+		}
+		else if(entrada2 == 2) {
+			Accesorio.visualizarTienda();
+			int opcion4 = scan.nextInt();
+}
+	}else {
+		System.out.println("No cuentas con Soles suficientes.");
+	}
+	
+}
+public static void visualizarAccesoriosComprados(ArrayList<Accesorio> accesoriosComprados) {
+	System.out.println("----Accesorios Comprados:-----");
+		System.out.println(accesoriosComprados.toString()); //TO DO
 }
 }
